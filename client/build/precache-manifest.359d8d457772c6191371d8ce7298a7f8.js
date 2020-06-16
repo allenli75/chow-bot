c8dedc5a714cc7d33753c6e28fa977769585cec0
Allen Li <46834662+allenli75@users.noreply.github.com>
2020-06-16 06:03:34 -0700

    Add files via upload

---
+++ client/build/precache-manifest.359d8d457772c6191371d8ce7298a7f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f64170b19158495bba13c6d8fca886c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2b06aa9937796eba665",
+    "url": "/static/css/main.c34fcebf.chunk.css"
+  },
+  {
+    "revision": "d18221dfb9676e7061b8",
+    "url": "/static/js/2.9ae38e67.chunk.js"
+  },
+  {
+    "revision": "0354a5099b1a6d3e50083b0a9287e2f8",
+    "url": "/static/js/2.9ae38e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2b06aa9937796eba665",
+    "url": "/static/js/main.f0aef4f1.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "bd874b6140fa2cb293e3f72c3d59613d",
+    "url": "/static/media/bgcrop1.bd874b61.png"
+  },
+  {
+    "revision": "b63fafa9bca61abcfb976699567726f2",
+    "url": "/static/media/chow.b63fafa9.png"
+  }
+]);
