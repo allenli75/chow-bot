25a495e4f0f2773a59b115b4218354753b40bbfc
Allen Li <allenli75@berkeley.edu>
2022-01-22 13:40:36 -0800

    Remove dotenv import

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: 'variables.env' });
+// require('dotenv').config({ path: 'variables.env' });
 
 const path = require('path');
 const bodyParser = require('body-parser');
